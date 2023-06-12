e987ccc5ee88c9d23eaa4194cc27312ef897c8bc
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-06-12 03:16:19 -0700

    Add files via upload

---
+++ VotingEligibility/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("What is your age?");
+int Age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Are you a citizen?");
+string Citizen = Convert.ToString(Console.ReadLine().ToUpper());
+
+if (Age >= 18)
+{
+    if (Citizen == "YES")
+    {
+        Console.WriteLine("You are eligible to vote.");
+    }
+    else
+    {
+        Console.WriteLine("Only citizens are eligible to vote.");       
+    }
+}
+else
+{
+    Console.WriteLine("You are not old enough to vote");    
+}
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
